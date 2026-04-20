4b827dff66098d3c55418d57e17bcd21732e3ffd
kiqkuk <kiqkuk@gmail.com>
2026-04-20 19:01:03 +0700

    feat: add nvim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- ~/.config/nvim/init.lua
+
+require("options")
+require("keymaps")
+require("diagnostics")
+require("lsp")
+require("pack")
+require("treesitter")
+
+-- plugins
+require("plugins.colors")
